84ac489a8e13df58d301cfbdacb037d09d0b8da4
Rubesh K Masilamani <rubeshmasilamani@gmail.com>
2023-04-25 21:45:00 +0100

    Testing

--- Test/com/assignment/FC/SessionLogicTest.java
+++ Test/com/assignment/FC/SessionLogicTest.java
@@ -1,43 +1,83 @@
 package com.assignment.FC;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-class SessionLogicTest {
 
-    @org.junit.jupiter.api.Test
-    void isBookingFull() {
-        SessionLogic sessionLogic = new SessionLogic("Yoga", "Monday", "Weekend", 5);
-        assertFalse(sessionLogic.isBookingFull());
-        CustomerLogic customer1 = new CustomerLogic("Harry", "harry@gmail.com");
+import java.util.ArrayList;
 
-        BookingFrame booking = new BookingFrame(new CustomerLogic("Harry", "harry@gmail.com"), new SessionLogic("Football", "Saturday", "Weekend 1", 4));
-        sessionLogic.addBookingList(booking);
-        assertFalse(sessionLogic.isBookingFull());
+import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.Test;
+class LessonTest {
+    @Test
+    public void testConstructor() {
+        String fitnessType = "Football";
+        String day = "Sunday";
+        String weekend = "No";
+        int price = 8;
 
-        sessionLogic.addBookingList(booking);
-        sessionLogic.addBookingList(booking);
-        sessionLogic.addBookingList(booking);
-        sessionLogic.addBookingList(booking);
-        sessionLogic.addBookingList(booking);
-        assertTrue(sessionLogic.isBookingFull());
+        SessionLogic lesson = new SessionLogic(fitnessType, day, weekend, price);
+
+        assertNotNull(lesson);
+        assertEquals(fitnessType, lesson.getFitnessType());
+        assertEquals(day, lesson.getDay());
+        assertEquals(weekend, lesson.getWeekend());
+        assertEquals(price, lesson.getPrice());
     }
 
-    @org.junit.jupiter.api.Test
-    void addBookingList() {
-        SessionLogic sessionLogic = new SessionLogic("Yoga", "Monday", "Weekend", 5);
-        BookingFrame booking = new BookingFrame(new CustomerLogic("Harry", "harry@gmail.com"), new SessionLogic("Football", "Saturday", "Weekend 1", 4));
-        sessionLogic.addBookingList(booking);
-        assertEquals(4, sessionLogic.getCapacity());
-        assertEquals(1, sessionLogic.getBookingList().size());
+    @Test
+    public void testIsBookingFull() {
+        String fitnessType = "Football";
+        String day = "Sunday";
+        String weekend = "No";
+        int price = 8;
+
+        SessionLogic lesson = new SessionLogic(fitnessType, day, weekend, price);
+        CustomerLogic customer = new CustomerLogic("Harry", "harry@gmail.com");
+
+        assertFalse(lesson.isBookingFull());
+
+
+        for (int i = 0; i < 10; i++) {
+            lesson.addBookingList(new BookingFrame(customer, lesson));
+        }
+        assertTrue(lesson.isBookingFull());
     }
 
-    @org.junit.jupiter.api.Test
-    void removeBookingList() {
-        SessionLogic sessionLogic = new SessionLogic("Yoga", "Monday", "Weekend", 5);
-        BookingFrame booking = new BookingFrame(new CustomerLogic("Harry", "harry@gmail.com"), new SessionLogic("Football", "Saturday", "Weekend 1", 4));
-        sessionLogic.addBookingList(booking);
-        sessionLogic.removeBookingList(booking);
-        assertEquals(5, sessionLogic.getCapacity());
-        assertEquals(0, sessionLogic.getBookingList().size());
+    @Test
+    public void testAddBookingList() {
+        String fitnessType = "Football";
+        String day = "Sunday";
+        String weekend = "No";
+        int price = 8;
+
+        SessionLogic lesson = new SessionLogic(fitnessType, day, weekend, price);
+        CustomerLogic customer = new CustomerLogic("Harry", "harry@gmail.com");
+        ArrayList<BookingFrame> bookingList = lesson.getBookingList();
+        assertNotNull(bookingList);
+        assertEquals(0, bookingList.size());
+
+        lesson.addBookingList(new BookingFrame(customer, lesson));
+        assertEquals(1, bookingList.size());
+    }
+
+    @Test
+    public void testRemoveBookingList() {
+        String fitnessType = "Football";
+        String day = "Sunday";
+        String weekend = "No";
+        int price = 8;
+
+        SessionLogic lesson = new SessionLogic(fitnessType, day, weekend, price);
+        CustomerLogic customer = new CustomerLogic("Harry", "harry@gmail.com");
+
+        ArrayList<BookingFrame> bookingList = lesson.getBookingList();
+        assertNotNull(bookingList);
+        assertEquals(0, bookingList.size());
+
+        BookingFrame booking = new BookingFrame(customer, lesson);
+        lesson.addBookingList(booking);
+        assertEquals(1, bookingList.size());
+
+        lesson.removeBookingList(booking);
+        assertEquals(0, bookingList.size());
     }
 }
